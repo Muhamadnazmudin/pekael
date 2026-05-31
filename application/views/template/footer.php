9c7030e8b4a6e05f7961b2bdc04639adaaaaad0b
smkclms <smkn1cilimus@gmail.com>
2026-05-31 16:18:34 +0700

    perbaikan footer

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -5,7 +5,31 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="text-center my-auto">
-            <span>Copyright &copy; Pekael <?= date('Y') ?></span>
+
+            <span>
+
+                Copyright &copy;
+
+                <a href="https://nazmu.web.id"
+                   target="_blank">
+
+                    Muhamad Nazmudin
+
+                </a>
+
+                <?= date('Y') ?>
+
+                •
+
+                <a href="https://www.profilsaya.my.id"
+                   target="_blank">
+
+                    Kocienten
+
+                </a>
+
+            </span>
+
         </div>
     </div>
 </footer>
