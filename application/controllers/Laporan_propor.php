3664c0c65b75b1b5c9f723133db233213d6d8b4f
smkclms <smkn1cilimus@gmail.com>
2026-06-06 11:57:28 +0700

    penambahan donload excel dan pdf pada laporan proporsional

--- application/controllers/Laporan_propor.php
+++ application/controllers/Laporan_propor.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Dompdf\Dompdf;
 class Laporan_propor extends CI_Controller
 {
     public function __construct()
@@ -47,12 +49,84 @@ class Laporan_propor extends CI_Controller
 }
 
     public function excel()
-    {
-        redirect('laporan_propor/rekap');
+{
+    $rekap = $this->M_rekap_pkl_propor->get_rekap();
+
+    $spreadsheet = new Spreadsheet();
+    $sheet = $spreadsheet->getActiveSheet();
+
+    foreach(range('A','H') as $col){
+        $sheet->getColumnDimension($col)->setAutoSize(true);
     }
 
-    public function pdf()
-    {
-        redirect('laporan_propor/rekap');
+    $sheet->setCellValue(
+        'A1',
+        'REKAP DATA PKL METODE PROPORSIONAL'
+    );
+
+    $sheet->fromArray([
+        'No',
+        'Nama Siswa',
+        'NISN',
+        'Kelas',
+        'Nama DUDI',
+        'Nomor MoU',
+        'Judul PKS',
+        'Pembimbing'
+    ], null, 'A3');
+
+    $row = 4;
+    $no  = 1;
+
+    foreach ($rekap as $r) {
+
+        $sheet->setCellValue('A'.$row, $no++);
+        $sheet->setCellValue('B'.$row, $r['nama']);
+        $sheet->setCellValue('C'.$row, $r['nisn']);
+        $sheet->setCellValue('D'.$row, $r['kelas']);
+        $sheet->setCellValue('E'.$row, $r['dudi']);
+        $sheet->setCellValue('F'.$row, $r['nomor_mou']);
+        $sheet->setCellValue('G'.$row, $r['judul_pks']);
+        $sheet->setCellValue('H'.$row, $r['pembimbing']);
+
+        $row++;
     }
+
+    header(
+        'Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    );
+
+    header(
+        'Content-Disposition: attachment; filename="rekap_pkl_proporsional.xlsx"'
+    );
+
+    $writer = new Xlsx($spreadsheet);
+    $writer->save('php://output');
+}
+public function pdf()
+{
+    $data['rekap'] =
+        $this->M_rekap_pkl_propor->get_rekap();
+
+    $html = $this->load->view(
+        'admin/laporan_propor/pdf',
+        $data,
+        true
+    );
+
+    $dompdf = new Dompdf();
+
+    $dompdf->loadHtml($html);
+    $dompdf->setPaper(
+        'A4',
+        'landscape'
+    );
+
+    $dompdf->render();
+
+    $dompdf->stream(
+        'rekap_pkl_proporsional.pdf',
+        ['Attachment' => true]
+    );
+}
 }
